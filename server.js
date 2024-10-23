ca26a15ea64c54324ce8f67ca067be6600092c0e
hub@2024 <rishirishitha333@gmail.com>
2024-10-23 10:58:57 +0530

    code-modified

--- server.js
+++ server.js
@@ -1,35 +1,3 @@
-// const express = require('express')
-// const app = express()
-
-// const {open} = require('sqlite')
-// const sqlite3 = require('sqlite3')
-// const path = require('path')
-
-// const dbPath = path.join(__dirname, 'transactions.db')
-// const bcrypt = require('bcrypt')
-// const jwt = require('jsonwebtoken')
-// app.use(express.json())
-
-// let db = null
-// const initateDBAndServer = async () => {
-//   try {
-//     db = await open({
-//       filename: dbPath,
-//       driver: sqlite3.Database,
-//     })
-
-//     app.listen(3000, () => {
-//       console.log('Server Running http://localhost:3000')
-//     })
-//   } catch (error) {
-//     console.log(`DB Error..${error.message}`)
-//     process.exit(1)
-//   }
-// }
-// initateDBAndServer()
-
-
-// app.js
 const express = require('express');
 const { open } = require('sqlite');
 const sqlite3 = require('sqlite3');
